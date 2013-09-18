f3f42978723aecd4a24b27c07c7c767d82202e08
ricardointeractive <ricardointeractive@hotmail.com>
2013-09-18 14:07:42 -0500

    Primera tarea de programacion

---
+++ personas.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: RicardoAlfonso
+ * Date: 17/09/2013
+ * Time: 08:06 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tarea1
+{
+	
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			variables p=new variables();
+			Console.WriteLine ("nombre de la persona");
+			p.nombre=Console.ReadLine();
+			Console.WriteLine ("nombre " +p.nombre);
+			Console.WriteLine ("correo de la persona");
+			p.correo=Console.ReadLine();
+			Console.WriteLine ("correo " +p.correo);
+			Console.WriteLine ("edad de la persona");
+			p.edad=int.Parse(Console.ReadLine());
+			Console.WriteLine ("edad " +p.edad);
+		}
+	}
+}
---
+++ variables.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: RicardoAlfonso
+ * Date: 17/09/2013
+ * Time: 08:10 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace tarea1
+{
+	
+	public class variables
+	{
+		public string nombre;
+		public string correo;
+		public int edad;
+		public variables()
+		{
+		}
+	}
+}
